540ae61ffaf94a931b8475a7afe44ad33395bc1f
riyasachdeva04 <riiya2048@gmail.com>
2024-12-20 22:25:18 +0530

    changes

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ if not os.path.exists(OUTPUT_PATH):
 
 def download_audio(url, video_num):
     try:
-        yt = YouTube(url)
+        yt = YouTube(url, use_po_token=True)
         audio_stream = yt.streams.filter(only_audio=True).first()
         
         audio_file = os.path.join(OUTPUT_PATH, f"video_{video_num}_{yt.title}.mp3")
